b155f1ab7187ed31e03ae8af4e12050ba92e1be3
Shriram Rajeshwar Nawathe <108632713+ShriramNawathe@users.noreply.github.com>
2023-03-21 12:32:10 +0530

    soln 3.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,56 @@
 import React, { useState } from "react";
 import "./../styles/App.css";
 
-const LoginForm = () => {
+const App = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!username || !password) {
-      setError("Please fill in both username and password fields.");
+      setError("Please fill in both fields.");
     } else {
-      // Submit the login form here.
+      // do something with username and password, e.g. send to server for authentication
+      console.log("Username:", username, "Password:", password);
     }
   };
 
   return (
-    <div className="login-form-container">
+    <div className="login-form">
+      <h1>Login Form</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        {error && <p id="errorMessage">{error}</p>}
-
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        {error && <div id="errorMessage">{error}</div>}
         <button type="submit">Login</button>
       </form>
-    </div>
-  );
-};
-
-const App = () => {
-  return (
-    <div>
-      <LoginForm />
     </div>
   );
 };
